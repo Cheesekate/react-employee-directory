10b9085e9752209a2c310d001b2da28f8084f37a
Kathryn wade <katewade0@gmail.com>
2020-07-06 18:43:08 -0700

    updates from CJ

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Jumbotron from './components/jumbotron/jumbotron';
+import Navbar from './components/navbar/navbar';
+import API from "./utils/API";
+import moment from "moment";
+import "./App.css"
+
+
+function App() {
+  return (
+    <>
+      <Jumbotron></Jumbotron>
+
+
+    </>
+  );
+}
+
+export default App;
