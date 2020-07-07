99155ac31de5b6ee2016f58010529fbf2a56a74e
Kathryn wade <katewade0@gmail.com>
2020-07-07 10:13:56 -0700

    final commit done on 7/6- notes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,20 +1,13 @@
-import React from 'react'
+import React from 'react';
+import React, { searchInput } from "react";
+import People from 
+
 
 export default function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="navbar-brand" href="#">Employee Directory</div>
-            <form class="form-inline my-2 my-lg-0">
-                <input
-                    className="form-control mr-sm-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search">
-                    name="search"
-                    onSearch={props.handleInputChange}
-                    value={props.search}
-                    /></input>
-            </form>
+
         </nav>
     )
 }
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,8 @@
+
 import axios from "axios";
 
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=X29S-H253-29AQ-08O2";
-
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-    getEmployee() {
-        return axios.get(BASEURL + APIKEY);
+    search: function () {
+        return axios.get("https://randomuser.me/api/?results=200&nat=us");
     }
 };
