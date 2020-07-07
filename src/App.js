cb4f3d52cd8ce1c0ae5d4ca0f835d0b9f4997358
Kathryn wade <katewade0@gmail.com>
2020-07-06 20:20:46 -0700

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -6,49 +6,69 @@ import API from "./utils/API";
 import moment from "moment";
 import "./App.css"
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={EmployeeDirectory} />
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
-
-export default App;
 
 
+export default class App extends React.Component {
+  state = {
+    results: [],
+    search: "",
+  };
 
-search = () => {
-  const { currentEmployee } = this.state;
-  let
-}
+  componentDidMount() {
+    API.getUser()
+      .then((res) =>
+        res.data.results.map((result) => ({
+          name: `${result.name.first}, ${result.name.last}`,
+          searchName: `${result.name.first}, ${result.name.last}`,
+          id: result.registered.date,
+          photo: result.picture.medium,
+          email: result.email,
+          phone: result.phone,
+          location: result.location.city,
+          dob: moment(result.dob.date).format("MM/DD/YYYY"),
+        }))
+      )
+      .then((newData) => this.setState({ results: newData }))
+      .catch((error) => console.log(error));
+  }
 
-render(){
-  const data = this.state.results;
-  const { currentEmployee } = this.state;
-
-  return (
-    data.length > 0 && (
-  )
-}
-
+  handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value.toLowerCase(),
+    });
+  };
 
 
-function App() {
-  return (
-    <>
-      <Jumbotron></Jumbotron>
 
 
-    </>
-  );
-}
 
-export default App;
+
+// search = () => {
+//   const { currentEmployee } = this.state;
+//   let
+// }
+
+// render(){
+//   const data = this.state.results;
+//   const { currentEmployee } = this.state;
+
+//   return (
+//     data.length > 0 && (
+//   )
+// }
+
+
+
+// function App() {
+//   return (
+//     <>
+//       <Jumbotron></Jumbotron>
+
+
+//     </>
+//   );
+// }
+
+// export default App;
